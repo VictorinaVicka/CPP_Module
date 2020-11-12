5d4a86efa129c0f0f4287c1c355ff058aa5aa80a
VictorinaVicka <city_br@mail.ru>
2020-11-12 11:07:02 +0300

    ex01

---
+++ CPP_Module01/ex01/ex01.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfarenga <tfarenga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 10:44:42 by tfarenga          #+#    #+#             */
+/*   Updated: 2020/11/12 11:06:17 by tfarenga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void memoryLeak()
+{
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
